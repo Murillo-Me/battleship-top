ad488c5ea80e74de5347d3b679815656985e9b0e
Murillo Mendes <murillo.mendes@troxbrasil.com.br>
2021-12-09 09:14:19 -0300

    Added some gameboardFactory methods

--- src/index.js
+++ src/index.js
@@ -24,7 +24,33 @@ const ship2 = shipFactory(2);
 ship1.hit(2);
 ship2.hit(7);
 
-// const gameboardFactory = (size) => {
+const gameboardFactory = (boardSize = 10) => {
+  // let player1Board =
 
-//   };
+  const placeShip = (shipSize, shipOrientation, xCoord, yCoord) => {
+    if (!shipOrientation) {
+      if (Math.random() < 0.5) shipOrientation = 'vertical';
+      else shipOrientation = 'horizontal';
+    }
+
+    if (!xCoord) {
+      do {
+        xCoord = getRandomIntInclusive(0, boardSize - 1);
+      } while (((boardSize - 1) - xCoord) > shipSize || shipOrientation === 'vertical');
+    }
+
+    if (!yCoord) {
+      do {
+        yCoord = getRandomIntInclusive(0, boardSize - 1);
+      } while (((boardSize - 1) - xCoord) > shipSize || shipOrientation === 'vertical');
+    }
+  };
+};
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 module.exports = { ship1, ship2 };
