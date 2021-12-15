edbe1cb23ecf771ced02eb27023b6f03624a3a2d
Murillo Mendes <murillomendes95@hotmail.com>
2021-12-15 13:48:35 -0300

    Started developing gameboardDOM module

--- src/index.js
+++ src/index.js
@@ -53,41 +53,45 @@ const shipFactory = (size, shipOrient) => {
 };
 
 const gameboardFactory = (boardSize = 10) => {
-  const board = create2DArray({ rows: boardSize, columns: boardSize, defaultValue: 0 });
+  const boardArray = create2DArray({ rows: boardSize, columns: boardSize, defaultValue: 0 });
   const boardID = gameboardList.length + 1;
   const shipsOnBoard = [];
 
   const emptyTheBoard = () => {
-    for (let i = 0; i < board.length; i += 1) {
-      for (let j = 0; j < board[i].length; j += 1) {
-        board[i][j] = 0;
+    for (let i = 0; i < boardArray.length; i += 1) {
+      for (let j = 0; j < boardArray[i].length; j += 1) {
+        boardArray[i][j] = 0;
       }
     }
-    return board;
+    return boardArray;
   };
 
-  const placeShip = (shipObject, yCoord, xCoord) => {
+  const placeShip = (shipObject, yCoord = false, xCoord = false) => {
     if (!yCoord) {
       do {
         yCoord = getRandomIntInclusive(0, boardSize - 1);
       } while (((boardSize - 1) - xCoord) > shipObject.size || shipObject.shipOrient === 'vertical');
     }
 
-    if (!xCoord) {
-      do {
-        xCoord = getRandomIntInclusive(0, boardSize - 1);
-      } while (((boardSize - 1) - xCoord) > shipObject.size || shipObject.shipOrient === 'vertical');
-    }
+    // if (!xCoord) {
+    //   do {
+    //     xCoord = getRandomIntInclusive(0, boardSize - 1);
+    //   } while (((boardSize - 1) - xCoord) > shipObject.size || shipObject.shipOrient === 'vertical');
+    // }
 
-    for (let i = 0; i < shipObject.size; i += 1) {
-      if (shipObject.shipOrient === 'vertical') {
-        board[yCoord + i][xCoord] = shipObject.shipID;
-      } else {
-        board[yCoord][xCoord + i] = shipObject.shipID;
-      }
-    }
+    // for (let i = 0; i < shipObject.size; i += 1) {
+    //   if (shipObject.shipOrient === 'vertical') {
+    //     boardArray[yCoord + i][xCoord] = shipObject.shipID;
+    //   } else {
+    //     boardArray[yCoord][xCoord + i] = shipObject.shipID;
+    //   }
+    // }
 
-    shipsOnBoard.push(shipObject);
+    // shipsOnBoard.push(shipObject);
+
+    console.log(shipObject);
+    console.log(yCoord);
+    console.log(xCoord);
   };
 
   // eslint-disable-next-line consistent-return
@@ -95,12 +99,12 @@ const gameboardFactory = (boardSize = 10) => {
     let shipHitPosition = 1;
     for (let i = 1; i < shipObject.size + 1; i += 1) {
       if (shipObject.shipOrient === 'vertical') {
-        if (board[yCoord - i][xCoord] === board[yCoord][xCoord]) {
+        if (boardArray[yCoord - i][xCoord] === boardArray[yCoord][xCoord]) {
           shipHitPosition += 1;
         } else return shipHitPosition;
       }
       if (shipObject.shipOrient === 'horizontal') {
-        if (board[yCoord][xCoord - i] === board[yCoord][xCoord]) {
+        if (boardArray[yCoord][xCoord - i] === boardArray[yCoord][xCoord]) {
           shipHitPosition += 1;
         } else return shipHitPosition;
       }
@@ -109,18 +113,18 @@ const gameboardFactory = (boardSize = 10) => {
 
   // eslint-disable-next-line consistent-return
   const receiveAttack = (shipArray, yAttackCoord, xAttackCoord) => {
-    if (board[yAttackCoord][xAttackCoord] < 0) return 'already tried';
+    if (boardArray[yAttackCoord][xAttackCoord] < 0) return 'already tried';
 
-    if (board[yAttackCoord][xAttackCoord] === 0) {
-      board[yAttackCoord][xAttackCoord] = -10;
+    if (boardArray[yAttackCoord][xAttackCoord] === 0) {
+      boardArray[yAttackCoord][xAttackCoord] = -10;
       return 'missed';
     }
 
-    if (board[yAttackCoord][xAttackCoord] > 0) {
-      const shipID = board[yAttackCoord][xAttackCoord];
+    if (boardArray[yAttackCoord][xAttackCoord] > 0) {
+      const shipID = boardArray[yAttackCoord][xAttackCoord];
       const hitPos = identifyShipHitPosition(shipArray[shipID], yAttackCoord, xAttackCoord);
       shipArray[shipID].hit(hitPos);
-      board[yAttackCoord][xAttackCoord] = -100;
+      boardArray[yAttackCoord][xAttackCoord] = -100;
       return 'success';
     }
   };
@@ -135,7 +139,7 @@ const gameboardFactory = (boardSize = 10) => {
   };
 
   return {
-    board, boardID, boardSize, placeShip, emptyTheBoard, receiveAttack, allShipsSunk, shipsOnBoard,
+    boardArray, boardID, boardSize, placeShip, emptyTheBoard, receiveAttack, allShipsSunk, shipsOnBoard,
   };
 };
 
@@ -147,7 +151,7 @@ const playerFactory = (playerID, name, isAI = false) => {
     score += 1;
   };
 
-  const AIPlay = (gameboard) => {
+  const computerPlay = (gameboard) => {
     let yCoord = 0;
     let xCoord = 0;
 
@@ -159,21 +163,48 @@ const playerFactory = (playerID, name, isAI = false) => {
     AIMemory.push(`${yCoord}, ${xCoord}`);
   };
 
+  const playerPlay = (gameboard) => {
+
+  };
+
   return {
-    playerID, name, isAI, score, win, AIPlay,
+    playerID, name, isAI, score, win, computerPlay,
   };
 };
 
-function createBoardDOM(gameboard) {
-  const gameboardElement = document.querySelector(`.gb-id-${gameboard.boardID}`);
-  for (let i = 0; i < gameboard.boardSize; i += 1) {
-    for (let j = 0; j < gameboard.boardSize; j += 1) {
-      const boardUnitElement = document.createElement('div');
-      boardUnitElement.classList.add('board-unit', `.r${i}-c${j}`);
-      gameboardElement.appendChild(boardUnitElement);
+const gameboardDOM = (function () {
+  function createBoardDOM(gameboard) {
+    const gameboardElement = document.querySelector(`.gb-id-${gameboard.boardID}`);
+    for (let i = 0; i < gameboard.boardSize; i += 1) {
+      for (let j = 0; j < gameboard.boardSize; j += 1) {
+        const boardUnitElement = document.createElement('div');
+        boardUnitElement.classList.add('board-unit', `.r${i}-c${j}`);
+        gameboardElement.appendChild(boardUnitElement);
+      }
     }
   }
-}
+
+  function toggleBoardPlayingState(gameboard) {
+    const allBoardUnitElements = document.querySelectorAll(`.gb-id-${gameboard.boardID} > .board-unit`);
+    allBoardUnitElements.forEach((unit) => unit.classList.toggle('playing-state'));
+  }
+
+  function renderShips(gameboardList) {
+    gameboardList.forEach((board) => {
+      console.log(board);
+      // board.boardArray.forEach((row) => {
+      //   console.log(row);
+      //   row.forEach((column) => {
+      //     console.log(column);
+      //     const shipUnitElement = document.querySelector(`.gb-id-${gameboard.boardID} > .r${row}-c${column}`);
+      //     console.log(shipUnitElement);
+      //   });
+      // });
+    });
+  }
+
+  return { createBoardDOM, toggleBoardPlayingState, renderShips };
+}());
 
 function receiveAttackDOM() {
 
@@ -184,11 +215,29 @@ gameboardList.push(gameboard1);
 const gameboard2 = gameboardFactory(10);
 gameboardList.push(gameboard2);
 
-createBoardDOM(gameboard1);
-createBoardDOM(gameboard2);
+const ship1 = shipFactory(5, 'vertical');
+shipList.push(ship1);
+const ship2 = shipFactory(4, 'vertical');
+shipList.push(ship2);
+const ship3 = shipFactory(3, 'horizontal');
+shipList.push(ship3);
+const ship4 = shipFactory(3);
+shipList.push(ship4);
+const ship5 = shipFactory(2);
+shipList.push(ship5);
 
-const allBoardUnits = document.querySelectorAll('.board-unit');
-allBoardUnits.forEach((unit) => unit.addEventListener('click', (e) => { e.currentTarget.style['background-color'] = 'white'; }));
+shipList.forEach((ship) => gameboard1.placeShip(ship));
+// shipList.forEach((ship) => gameboard2.placeShip(ship));
+
+gameboardDOM.createBoardDOM(gameboard1);
+gameboardDOM.createBoardDOM(gameboard2);
+
+// gameboardDOM.toggleBoardPlayingState(gameboard2);
+// console.log(gameboardList);
+// gameboardDOM.renderShips(gameboardList);
+
+// const allBoardUnits = document.querySelectorAll('.board-unit');
+// allBoardUnits.forEach((unit) => unit.addEventListener('click', (e) => { e.currentTarget.style['background-color'] = 'white'; }));
 
 export { shipFactory, gameboardFactory, playerFactory };
 
